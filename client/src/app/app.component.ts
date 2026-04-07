350839b93928ef2cd1ab2efc984bdd9b2621f937
Your Name <mauclert@gmail.com>
2026-04-07 13:42:24 +0200

    integrate contact api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,23 +1,3 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { Header } from './header/header';
-// import { Footer } from './footer/footer';
-// import { OfflineBannerComponent } from './components/offline-banner/offline-banner.component';
-// import { routeTransitionAnimations } from './app.animations';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet, Header, Footer, OfflineBannerComponent],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.scss',
-//   animations:[routeTransitionAnimations]
-
-// })
-// export class AppComponent {
-//   title = 'test-ssr';
-// }
-
 import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
